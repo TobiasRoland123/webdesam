8ad4ef2eae13b334a666536f8743638a7cc402f8
Samuel Tobias Roland Uyet <samu1493@stud.kea.dk>
2023-07-04 23:55:33 +0200

    Contact

--- src/pages/index.js
+++ src/pages/index.js
@@ -64,7 +64,7 @@ export default function Home() {
         <h2>Contact me</h2>
 
         <form
-          name="contact"
+          name="Contact"
           method="POST"
           data-netlify="true"
           onSubmit="submit"
