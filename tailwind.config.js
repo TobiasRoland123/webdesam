4de1a76f1cf5d2ce3e0bc1da3df9db893fbeffb0
Samuel Tobias Roland Uyet <samu1493@stud.kea.dk>
2023-08-14 12:52:06 +0200

    screen breakpoint edits

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,22 @@ module.exports = {
       ubuntu: ["ubuntu", "sans-serif"],
       "Source Sans 3": ["Source Sans 3", "sans-serif"], // Ensure fonts with spaces have " " surrounding it.
     },
+    screens: {
+      sm: "850px",
+      // => @media (min-width: 640px) { ... }
+
+      md: "950px",
+      // => @media (min-width: 768px) { ... }
+
+      lg: "1024px",
+      // => @media (min-width: 1024px) { ... }
+
+      xl: "1280px",
+      // => @media (min-width: 1280px) { ... }
+
+      "2xl": "1536px",
+      // => @media (min-width: 1536px) { ... }
+    },
     extend: {
       colors: {
         col: {
