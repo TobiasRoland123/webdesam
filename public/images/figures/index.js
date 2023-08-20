df46999597d3881563c4ed1632e4867b2a8b1815
Tobias <samu1493@stud.kea.dk>
2023-08-20 22:00:14 +0200

    favicon added

--- public/images/figures/index.js
+++ public/images/figures/index.js
@@ -2,3 +2,4 @@ export { default as PersonSvg } from "./person.svg";
 export { default as creativeSvg } from "./creative.svg";
 export { default as supportSvg } from "./support.svg";
 export { default as toolsSvg } from "./tools.svg";
+// export { default as favicon_512 } from "./favicon_512.png";
